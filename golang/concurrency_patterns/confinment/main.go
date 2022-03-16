5221ea7532bc10bdc0098b6040075a36108782dd
Ahmed Elbarky <elbarky.dev@gmail.com>
2022-03-16 12:36:36 +0200

    or functionality with channels

--- golang/concurrency_patterns/confinment/main.go
+++ golang/concurrency_patterns/confinment/main.go
@@ -7,6 +7,37 @@ import (
 func main() {
 }
 
+func orChannel() {
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+}
+
 func orchastrated_routine() {
 	// <- marks the function as a recieve only
 	dowork := func(
